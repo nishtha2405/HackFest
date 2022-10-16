40c9bed262a7d85759932f554cdded3714f2b098
AmarnathGowda <115018321+AmarnathGowda@users.noreply.github.com>
2022-10-16 11:13:26 +0530

    Added code for PathSum
    Added code for PathSum. This is one of the most asked interview question.

---
+++ Code for good/PathSum.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <map>
+#include <cmath>
+#include <conio.h>
+#include <queue>
+typedef long long ll;
+using namespace std;
+int main()
+{
+    getch();
+}
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    bool hasPathSum(TreeNode *root, int targetSum)
+    {
+        if(!root)
+            return false;
+        targetSum -= root->val;
+        if(targetSum==0&&root->left==NULL&&root->right==NULL)
+            return true;
+        return hasPathSum(root->left,targetSum)||hasPathSum(root->right,targetSum);
+    }
+};
