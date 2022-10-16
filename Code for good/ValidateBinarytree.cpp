f152b005844449e8c71aa1eff6e1e0672d69ae52
unknown <nancykumari620@gmail.com>
2022-10-16 15:13:01 +0530

    ValidateBinaryTree

---
+++ Code for good/ValidateBinarytree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBST(TreeNode*root,long min,long max){
+       if(root==NULL)
+            return 1;
+      else  if(root->val<=min||root->val>=max)
+            return 0;
+        
+        return isBST(root->left,min,root->val)&&isBST(root->right,root->val,max);
+        
+    }
+    bool isValidBST(TreeNode* root) {
+        if(root==NULL)
+            return 1;
+        long min=LONG_MIN;
+        long max=LONG_MAX;
+        return isBST(root,min,max);
+        
+    }
+};
